2ef3b110e1a68ee80625e832a60459c60384a139
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2024-07-31 21:40:00 +0000

    ssssss

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixa-alternativas");
+/*const caixaAlternativas = document.querySelector(".caixa-alternativas");*/
 const caixaResultado = document.querySelector(".resultado-1");
 const caixaResultado2 = document.querySelector(".resultado-2");
 const textoResultado = document.querySelector(".textoResultado");
 const botao1 = document.querySelector(".caixa-alternativas .botao_1");
-const botao2 = document.querySelector(".caixa-alternativas .botao_2");
+/*const botao2 = document.querySelector(".caixa-alternativas .botao_2");*/
 console.log(botao1);
 
 /*Interceptar o click do botão*/
