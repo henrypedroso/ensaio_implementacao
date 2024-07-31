801291738011ef85594941c189e4ab175997e8db
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2024-07-31 21:51:09 +0000

    ssssssssssssssss

--- main.js
+++ main.js
@@ -18,8 +18,8 @@ botao1.addEventListener("click", function(){
 
 /*botao2.addEventListener("click", function(){
     console.dir(caixaResultado); /*Mostra o elemento html em formato de objeto*/
-    /*caixaResultado2.classList.add("mostrar-caixa-resultado");*/
+    /*caixaResultado2.classList.add("mostrar-caixa-resultado");
     
-})
+})*/
 
 
